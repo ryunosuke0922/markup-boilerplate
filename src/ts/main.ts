2cdd2361f0dd9c8c9bd0c65adcc7c714e0c565df
ryunosuke0922 <ryumaeda0922@gmail.com>
2022-09-28 00:23:29 +0900

    fix

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,31 +3,31 @@ import * as sourceMapSupport from 'source-map-support';
 sourceMapSupport.install();
 
 class ScrollFade {
-	constructor(targetClass = '.js-scroll-fade') {
-		const animationClass = 'is-animation';
+  constructor(targetClass = '.js-scroll-fade') {
+    const animationClass = 'is-animation';
 
-		const section = document.querySelectorAll(`${targetClass}:not(.is-animation)`);
-		if (section.length === null) {
-			return;
-		}
+    const section = document.querySelectorAll(`${targetClass}:not(.is-animation)`);
+    if (section.length === null) {
+      return;
+    }
 
-		const controller = new ScrollMagic.Controller();
-		for (let i = 0; i < section.length; i++) {
-			const scene = new ScrollMagic.Scene({
-				triggerElement: section[i],
-				triggerHook: 'onEnter',
-				reverse: false,
-				offset: 200,
-			}).addTo(controller);
-			scene.on('enter', () => {
-				section[i].classList.add(animationClass);
-			});
-			scene.on('leave', () => {
-				section[i].classList.remove(animationClass);
-			});
-			// scene.on('end', () => {});
-		}
-	}
+    const controller = new ScrollMagic.Controller();
+    for (let i = 0; i < section.length; i++) {
+      const scene = new ScrollMagic.Scene({
+        triggerElement: section[i],
+        triggerHook: 'onEnter',
+        reverse: false,
+        offset: 200,
+      }).addTo(controller);
+      scene.on('enter', () => {
+        section[i].classList.add(animationClass);
+      });
+      scene.on('leave', () => {
+        section[i].classList.remove(animationClass);
+      });
+      // scene.on('end', () => {});
+    }
+  }
 }
 
 new ScrollFade();
